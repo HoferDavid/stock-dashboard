ce2327a11322de56455be3da3275653391c22b8a
David Hofer <chassis.poppets0x@icloud.com>
2024-10-16 14:05:04 +0200

    add environments files

---
+++ src/app/services/stockdata.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StockDataService {
+  // private apiKey = 'AIzaSyBmqC_yDEf1pISefZEUyoEkaJ_t7KTl0vU';
+  private apiKey = environment.apiKey;
+  private spreadsheetId = '1m3h9Xce3SRMaq20li2Qg4HuSZowwxQSd9FkQmeuI7Dw';
+
+  constructor(private http: HttpClient) {}
+
+  getStockData(sheetName: string): Observable<any> {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetId}/values/${sheetName}!A:Z?key=${this.apiKey}`;
+    return this.http.get(url);
+  }
+}
