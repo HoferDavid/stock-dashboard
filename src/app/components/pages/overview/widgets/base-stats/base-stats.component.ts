5a15f295e4c84114cdd729798d9054f68fee1a7c
David Hofer <chassis.poppets0x@icloud.com>
2024-10-23 17:51:14 +0200

    feat: quarterFormat pipe

--- src/app/components/pages/overview/widgets/base-stats/base-stats.component.ts
+++ src/app/components/pages/overview/widgets/base-stats/base-stats.component.ts
@@ -3,11 +3,12 @@ import { MatIcon } from '@angular/material/icon';
 import { OverviewWidget } from '../../../../../interfaces/overview';
 import { CommonModule } from '@angular/common';
 import { BillionFormatPipe } from '../../../../../pipes/billion-format.pipe';
+import { QuarterFormatPipe } from '../../../../../pipes/quarter-format.pipe';
 
 @Component({
   selector: 'app-base-stats',
   standalone: true,
-  imports: [MatIcon, CommonModule, BillionFormatPipe],
+  imports: [MatIcon, CommonModule, BillionFormatPipe, QuarterFormatPipe],
   templateUrl: './base-stats.component.html',
   styleUrl: './base-stats.component.scss'
 })
---
+++ src/app/pipes/quarter-format.pipe.ts
@@ -0,0 +1,29 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'quarterFormat',
+  standalone: true
+})
+export class QuarterFormatPipe implements PipeTransform {
+
+  transform(value: string | undefined): string {
+    if (!value) {
+      return 'undefined';
+    }
+
+    const regexCompact = /^(\d{2})Q(\d)$/; // Match "24Q3"
+    const regexExtended = /^Q(\d) (\d{4})$/; // Match "Q3 2024"
+
+    if (regexCompact.test(value)) {
+      const [, year, quarter] = value.match(regexCompact)!;
+      return `Q${quarter} 20${year}`;
+    }
+
+    if (regexExtended.test(value)) {
+      return value;
+    }
+
+    return value;
+  }
+
+}
